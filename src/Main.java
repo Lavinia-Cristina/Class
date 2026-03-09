54299401268427444dfbd116a7311186d3a44b52
Lavinia <laviniacristina.botea@ulbsibiu.ro>
2026-03-09 22:49:57 +0200

    class

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
 
+IO.print("Clasa da aceste date: ");
+IO.println();
 
+ Student stud1= new Student( 527, "Botea", "Lavinia", "ISMzi");
+ IO.println(stud1);
 
 }
