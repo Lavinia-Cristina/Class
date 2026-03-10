fc28dc30a941807a3a522906cea978e06fdd5197
Lavinia <laviniacristina.botea@ulbsibiu.ro>
2026-03-10 11:06:58 +0200

    lab2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,52 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
 
+void main() {
+//LAB1
 IO.print("Clasa da aceste date: ");
 IO.println();
 
  Student stud1= new Student( 527, "Botea", "Lavinia", "ISM21/2");
  IO.println(stud1);
 
+ IO.println();
+ IO.println();
+
+
+ //LAB2
+
+
+
+ List<Integer> x = new ArrayList();
+
+ x.add(1);
+ x.add(10);
+ x.add(3);
+ x.add(5);
+ x.add(2);
+
+
+
+ List<Integer> y = new ArrayList();
+
+ y.add(1);
+ y.add(2);
+ y.add(3);
+ y.add(5);
+ y.add(4);
+ y.add(6);
+ y.add(7);
+
+ Collections.sort(x);
+ Collections.sort(y);
+
+ List<Integer> xPlusY = new ArrayList(); //a
+ xPlusY.addAll(x);
+ xPlusY.addAll(y);
+ Collections.sort(xPlusY);
+ IO.print(xPlusY);
+
+
+ Set<Integer> zSet = new TreeSet(); //b
+ List<Integer> xMinusY = new ArrayList();//c
+ int p = 4;
+ List<Integer> xPlusYLimitedByP = new ArrayList(); //d
 }
